7336c4d019840a39e694357b4d650f03e9115d1d
Neil Saunders <neilfws@gmail.com>
2017-03-03 15:50:56 +1100

    Added nhmrcBRA documentation

---
+++ nhmrcData/R/nhmrcBRA.R
@@ -0,0 +1,14 @@
+#' NHMRC Expenditure by Broad Research Area.
+#'
+#' Summary of expenditure by broad research area and year.
+#'
+#' @source https://www.nhmrc.gov.au/_files_nhmrc/file/grants/dataset/2016/all_grants_2000-2016.xlsx
+#' @format Data frame with columns
+#' \describe{
+#' \item{calendar_year}{Calendar year}
+#' \item{basic_research_area}{Basic research area (BRA)}
+#' \item{value}{Expenditure for the BRA}
+#' }
+#' @examples
+#'   nhmrcBRA
+"nhmrcBRA"
