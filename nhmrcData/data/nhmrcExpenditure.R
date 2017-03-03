abf8f63c1efa434d1380b6578e095213d37dcc84
Neil Saunders <neilfws@gmail.com>
2017-03-03 15:25:14 +1100

    First R documentation file initial commit

---
+++ nhmrcData/data/nhmrcExpenditure.R
@@ -0,0 +1,14 @@
+#' NHMRC Expenditure.
+#'
+#' Summary of total expenditure and active grants by year.
+#'
+#' @source https://www.nhmrc.gov.au/_files_nhmrc/file/grants/dataset/2016/all_grants_2000-2016.xlsx
+#' @format Data frame with columns
+#' \describe{
+#' \item{calendar_year}{Calendar year}
+#' \item{total_expenditure}{Total expenditure for the calendar year}
+#' \item{active_grants}{Active grants for the calendar year}
+#' }
+#' @examples
+#'   nhmrcExpenditure
+"nhmrcExpenditure"
