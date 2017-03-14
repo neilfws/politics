889cf06f7741a97598922f1c8cdbd591f19797ec
Neil Saunders <neilfws@gmail.com>
2017-03-15 07:30:36 +1100

    Added documentation for nhmrcOutcomesGenderRatesCMS

---
+++ nhmrcData/R/nhmrcOutcomesGenderRatesCMS.R
@@ -0,0 +1,17 @@
+#' NHMRC Outcomes Gender Rates CMS.
+#'
+#' NHMRC project grants new investigators funding rates clin med & sci.
+#'
+#' @source https://www.nhmrc.gov.au/_files_nhmrc/file/research/women_in_science/project_grant_funded_rate_gender_new_investigator_and_teaching_load_140218.xlsx
+#' @format Data frame with columns
+#' \describe{
+#' \item{year}{Year}
+#' \item{ci}{CI group (CIA, CIB...)}
+#' \item{gender}{Gender}
+#' \item{stage}{Stage (applied, funded)}
+#' \item{value}{Count for each stage}
+#' }
+#' @examples
+#'   nhmrcOutcomesGenderRatesCMS %>% glimpse()
+"nhmrcOutcomesGenderRatesCMS"
+
