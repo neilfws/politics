03c77fb6aec9f3fa458ecf5f29d469b624e389ac
Neil Saunders <neilfws@gmail.com>
2017-03-14 22:08:51 +1100

    Code to generate nhmrcOutcomesGenderRatesBS

--- nhmrcData/data-raw/process_nhmrc_files.R
+++ nhmrcData/data-raw/process_nhmrc_files.R
@@ -159,6 +159,18 @@ save(nhmrcOutcomesGenderRates, file = "data/nhmrcOutcomesGenderRates.rda")
 ## sheet = AVERAGE YEARS POST DOC
 
 ## sheet = FUNDED RATES - BASIC SCIENCE
+nhmrcOutcomesGenderRatesBS <- project_grant_funded_rate_gender_new_investigator_and_teaching_load_140218_xlsx %>%
+  read_excel(sheet = " FUNDED RATES - BASIC SCIENCE", col_names = FALSE, skip = 3) %>%
+  remove_empty_cols() %>%
+  filter(X0 != "Grand Total") %>%
+  mutate(year = ifelse(grepl("^C", X0), NA, X0)) %>%
+  fill(year) %>%
+  mutate(year = as.integer(year)) %>%
+  filter(!grepl("^20", X0)) %>%
+  select(year, ci = X0, women_applied = X1, women_funded = X2, men_applied = X5, men_funded = X6) %>%
+  gather(stage, value, -ci, -year) %>%
+  separate(stage, into = c("gender", "stage"), sep = "_")
+save(nhmrcOutcomesGenderRatesBS, file = "data/nhmrcOutcomesGenderRatesBS.rda")
 
 ## sheet = FUNDED RATES - CLIN MED & SCI
 
