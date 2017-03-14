22c70e4dd8be2b68759e8fe184c527e715615de9
Neil Saunders <neilfws@gmail.com>
2017-03-15 07:25:49 +1100

    Added documentation for nhmrcOutcomesGenderPostDoc

---
+++ nhmrcData/R/nhmrcOutcomesGenderPostDoc.R
@@ -0,0 +1,18 @@
+#' NHMRC Outcomes Gender PostDoc.
+#'
+#' NHMRC project grants new investigators average years postdoc at time of application.
+#'
+#' @source https://www.nhmrc.gov.au/_files_nhmrc/file/research/women_in_science/project_grant_funded_rate_gender_new_investigator_and_teaching_load_140218.xlsx
+#' @format Data frame with columns
+#' \describe{
+#' \item{year}{Year}
+#' \item{ci}{CI group (CIA, CIB...)}
+#' \item{gender}{Gender}
+#' \item{profiles}{Number of applicant profiles}
+#' \item{stage}{Stage (applied, funded, unfunded)}
+#' \item{value}{Average years postdoc for stage}
+#' }
+#' @examples
+#'   nhmrcOutcomesGenderPostDoc %>% glimpse()
+"nhmrcOutcomesGenderPostDoc"
+
